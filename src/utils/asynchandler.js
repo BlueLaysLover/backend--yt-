c9648eb8c3e6e930377dc300eca101e790484cfe
Urvik Sharma <sharmaurvik2706@gmail.com>
2025-02-03 23:37:24 +0530

    api , utils

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,17 @@
+const asynchandler = (fn) => async(req , res , next) => {
+    try {
+        await fn(req , res, next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success:false, 
+            message:error.message
+        
+        })
+    }
+}
+
+
+
+
+
+export{asynchandler}
