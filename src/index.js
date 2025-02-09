adbc00ee2be23784e47f47c77d97cbeae3d08ba5
Urvik Sharma <sharmaurvik2706@gmail.com>
2025-02-09 12:20:16 +0530

    till routes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { DB_NAME } from "./constants.js";
 import express from "express"
 import connectDB from "./db/index.js";
 
+import {app} from './app.js'
+
 dotenv.config({
     path: './env'
 })
